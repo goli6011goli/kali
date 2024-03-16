3194f753f8c13b69f1dfef7e35cd8671557d17cc
Goli <161311891+goli6011goli@users.noreply.github.com>
2024-03-16 10:39:33 +0330

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -1,28 +1,61 @@
-sudo apt update
+echo                          *&@@@@@@@@@@@@@@@@@@@@@@@@@@@@&*                       
+                    (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@/                  
+                .@@@@@@@@@&.                                                    
+              &@@@@@@@.                                                         
+            @@@@@@@         ,&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&          
+          #@@@@@%       %@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@/        
+         @@@@@@      /@@@@@@@&,                                    @@@@@&       
+        @@@@@#     *@@@@@@,                                         &@@@@@      
+       &@@@@%     %@@@@@         /@@@.      .@@@/        ,@@@@@#     &@@@@%     
+      ,@@@@@     #@@@@@          @@@@@      @@@@@          @@@@@/     @@@@@.    
+      %@@@@(     @@@@@      ,%&&&@@@@@&&&#         (%,      @@@@@     %@@@@(    
+      %@@@@*    .@@@@@     @@@@@@@@@@@@@@@@(     ,@@@@@     @@@@@     (@@@@#    
+      #@@@@#     @@@@@       ****@@@@@***,         .*      .@@@@@     %@@@@(    
+      .@@@@@     (@@@@@          @@@@@      @@@@@          @@@@@*     @@@@@     
+       &@@@@&     #@@@@@*        .@@&        &@@.        /@@@@@/     @@@@@#     
+        @@@@@%                                         %@@@@@@      @@@@@&      
+         @@@@@@                                   ,%@@@@@@@@       @@@@@%       
+          *@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@,       @@@@@@,        
+            &@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@/         ,@@@@@@%          
+                                                           #@@@@@@@/            
+                                                     .#@@@@@@@@@%               
+                    ,@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&.                  
+                          /@@@@@@@@@@@@@@@@@@@@@@@@@@@@/                         
 
-sudo apt list --upgradable
-
-sudo apt upgrade -y
-
-sudo apt autoremove -y
-
-############################
 
 sudo apt install figlet -y
 
+figlet install figlet Done -f small | lolcat
+
 sudo apt install lolcat -y
+
+figlet install lolcat Done -f small | lolcat
+
+######################################################
+
+sudo apt update
+
+figlet update Done -f small | lolcat
+
+sudo apt list --upgradable
+
+figlet list upgrade Done -f small | lolcat
+
+sudo apt upgrade -y
+
+figlet upgrade Done -f small | lolcat
+
+sudo apt autoremove -y
+
+figlet autoremove Done -f small | lolcat
+
+############################
 
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-sudo cp ./apt.up /usr/bin/apt.up
-
-############################################################
-
-sudo apt install kali-grant-root -y
-
 sudo chsh -s /usr/bin/zsh
 
-sudo echo sudo su >> ../../.bashrc
+echo sudo su >> ~/.bashrc
 
 sudo cp ./.zshrc /root/.zshrc
 
@@ -33,5 +66,11 @@ sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUS
 sudo git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
 
 sudo git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+
+#####################################
+
+sudo cp ./apt.up /usr/bin/apt.up
+
+sudo apt install kali-grant-root -y
 
 sudo dpkg-reconfigure kali-grant-root
